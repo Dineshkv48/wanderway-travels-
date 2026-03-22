bfa25f3db26a27bb6df29754a796750529a02c81
Dineshkv48 <dinesh.kv.2024.aiml@rajalakshmi.edu.in>
2026-03-22 17:39:38 +0530

    Use environment variable for server port

--- backend/server.js
+++ backend/server.js
@@ -84,7 +84,7 @@ app.delete('/api/trips/:id', (req, res) => {
     });
 });
 
-const PORT = 5000;
+const PORT =  process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Backend server running on http://localhost:${PORT}`);
+    console.log("Server running on port " + PORT);
 });
